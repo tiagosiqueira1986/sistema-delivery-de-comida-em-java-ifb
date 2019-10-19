e8fb1c0d6dabcb207bdd0366bc20b6b7418e791a
Tiago Siqueira Ayres <tiagogunit@hotmail.com>
2019-10-19 16:06:08 -0300

    Design da tela de cadastro de Categorias e abertura da janela a partir da Tela principal

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -36,7 +36,7 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemCategoriaAcao() {
-		System.out.println("onMenuItemCategoriaAcao");
+		carregarTela("/gui/ListaDeCategorias.fxml");
 	}
 	
 	@FXML
